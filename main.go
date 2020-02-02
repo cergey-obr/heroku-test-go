2e7b6f6d09915ed4ffaa5a6e612c6b5206b5da20
Sergey Obraztsov <cergey.obr@gmail.com>
2020-02-02 13:52:25 +0400

    use system port

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"github.com/labstack/echo"
 	"net/http"
+	"os"
 )
 
 func main() {
@@ -11,6 +13,11 @@ func main() {
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		fmt.Println("$PORT not set")
+	}
 
 	e.Logger.Fatal(e.Start(":1323"))
 }
