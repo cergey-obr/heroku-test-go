b3c5b64d4009cd7b9d1a279dc45c58a164a68e00
Sergey Obraztsov <cergey251@yandex.ru>
2020-02-04 12:29:07 +0400

    payload

--- main.go
+++ main.go
@@ -1,18 +1,34 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/labstack/echo"
 	"os"
 )
 
+type Payload struct {
+	Token   string `json:"token"`
+	HookUrl string `json:"response_url"`
+	Actions []struct {
+		Id string `json:"action_id"`
+	} `json:"actions"`
+}
+
 func main() {
 	e := echo.New()
 
 	e.POST("/", func(c echo.Context) error {
-		//payload := map[string]interface{}{}
-		data := c.FormValue("payload")
-		fmt.Println(data)
+		var payload Payload
+		formData := []byte(c.FormValue("payload"))
+
+		if err := json.Unmarshal(formData, &payload); err != nil {
+			return err
+		}
+
+		fmt.Println(payload.Token)
+		fmt.Println(payload.HookUrl)
+		fmt.Println(payload.Actions[0].Id)
 
 		return c.JSON(200, echo.Map{})
 	})
